09a14e0e32768e594f8c472c480e8d531ee5ddb8
Andrej Jovanovic <andrej@jovanovic.co.za>
2026-03-16 20:20:42 +0000

    Improve site indexing

--- _notes/ngram.md
+++ _notes/ngram.md
@@ -1,6 +1,15 @@
 ---
 layout: notes
 title: ngram language modelling tutorial
+description: Tutorial session delivered with the University of Cape Town AI Society on n-gram language modelling.
+seo_type: VideoObject
+image: /assets/img/detailed_front.jpeg
+video_name: N-gram Language Modelling Tutorial
+video_description: Tutorial recording on language modelling with n-gram models delivered with the University of Cape Town AI Society.
+video_thumbnail: /assets/img/detailed_front.jpeg
+video_upload_date: "2024-01-01"
+video_embed_url: https://drive.google.com/file/d/1vw-pQ8U9ht3O5uHyH2rGYlUJgnK1VASb/preview
+video_content_url: https://drive.google.com/file/d/1vw-pQ8U9ht3O5uHyH2rGYlUJgnK1VASb/view
 ---
 Partnering with the University of Cape Town AI Society. I delivered a brief tutorial on language modelling using n-gram models.
 
--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -4,27 +4,57 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+function collapseNavbarStyle() {
+    if ($('.navbar').offset().top > 50) {
+        $('.navbar-fixed-top').addClass('top-nav-collapse');
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-fixed-top').removeClass('top-nav-collapse');
     }
+}
+
+function closeResponsiveMenu() {
+    var $menu = $('.navbar-main-collapse');
+    var $toggle = $('.navbar-toggle:visible');
+
+    if ($menu.hasClass('in') && $toggle.length) {
+        $toggle.trigger('click');
+    }
+}
+
+$(window).on('scroll', function () {
+    collapseNavbarStyle();
+    closeResponsiveMenu();
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+$(function () {
+    collapseNavbarStyle();
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    $('a.page-scroll').on('click', function (event) {
+        var targetSelector = $(this).attr('href') || '';
+
+        // Only hijack same-page hash links for smooth scrolling.
+        if (targetSelector.charAt(0) !== '#') {
+            closeResponsiveMenu();
+            return;
+        }
+
+        var $target = $(targetSelector);
+        if (!$target.length) {
+            closeResponsiveMenu();
+            return;
+        }
+
+        $('html, body').stop().animate({
+            scrollTop: $target.offset().top
+        }, 1500, 'easeInOutExpo');
+
+        event.preventDefault();
+        closeResponsiveMenu();
+    });
+
+    // Closes the responsive menu on menu item click.
+    $('.navbar-collapse ul li a').on('click', function () {
+        closeResponsiveMenu();
+    });
 });
